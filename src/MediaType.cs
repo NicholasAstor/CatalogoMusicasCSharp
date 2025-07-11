f93714d57048c9045e8eee8c68933e8af37eab51
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-07-11 11:51:29 -0300

    Media Type class

---
+++ src/MediaType.cs
@@ -0,0 +1,23 @@
+namespace src;
+
+public abstract class MediaType
+{
+    public string? Year { get; }
+
+    public string? Name { get; }
+
+    //duration
+    public Author Author { get; } // a person or band
+
+    public long Length { get; set; } //  Length of an album in seconds
+
+    public MediaType(string year, string name, Author author)
+    {
+        Year = year;
+        Name = name;
+        Author = author;
+    }
+
+    public abstract string ToString();
+}
+
